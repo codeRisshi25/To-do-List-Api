92808f0b6942ecb691f46d8a93cd19154e1d4535
Risshi25 <risshirajsen@gmail.com>
2025-05-01 13:03:37 +0530

    refactor: improve authentication logic and error handling in controllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,35 +1,127 @@
-import User from '../models/User.js';
+import User from "../models/User.js";
 
-const signupController = async (req, res) => {
-    try {
-        const data = req.body;
+const getData = (reqBody, isLogout = false) => {
+  if (!reqBody || !reqBody.username || (!isLogout && !reqBody.password)) {
+    const error = isLogout
+      ? "username is required"
+      : "username and password are required";
+    return { error };
+  }
 
-        if (!data || !data.username || !data.password) {
-            return res.status(400).json({ success: false, error: "username and password are required" });
-        }
-
-        const { username, password } = data;
-
-        const existingUser = await User.checkIfExists(username);
-        if (existingUser) {
-            return res.status(409).json({ success: false, error: "User already exists" });
-        }
-
-        const user = await User.create({ username, password });
-        if (!user) {
-            return res.status(500).json({ success: false, error: "Failed to create user" });
-        }
-        console.log("user created successfully", user.uid )
-        return res.status(201).json({ success: true, message: "User created successfully", userId: user.uid });
-    } catch (err) {
-        console.error("Error during signup:", err);
-        return res.status(500).json({ success: false, error: "Internal server error" });
-    }
+  const { username, password } = reqBody;
+  return isLogout ? { username } : { username, password };
 };
 
-const loginController = () =>{}
-const logoutController = () => {}
+// SignUp logic
+const signupController = async (req, res) => {
+  try {
+    const { username, password, error } = getData(req.body);
+    if (error) {
+      return res.status(400).json({ success: false, error });
+    }
+    const existingUser = await User.checkIfExists(username);
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ success: false, error: "User already exists" });
+    }
 
+    const user = await User.create({ username, password });
+    if (!user) {
+      return res
+        .status(500)
+        .json({ success: false, error: "Failed to create user" });
+    }
+    console.log("user created successfully", user.uid);
+    return res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      userId: user.uid,
+    });
+  } catch (err) {
+    console.error("Error during signup:", err);
+    return res
+      .status(500)
+      .json({ success: false, error: "Internal server error" });
+  }
+};
 
+// Login
+const loginController = async (req, res) => {
+  try {
+    const { username, password, error } = getData(req.body);
+    if (error) {
+      return res.status(400).json({ success: false, error });
+    }
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      return res
+        .status(409)
+        .json({ success: false, error: "User does not exist" });
+    }
+    if (!user.validatePassword(password)) {
+      return res
+        .status(401)
+        .json({ success: false, error: "Invalid password" });
+    }
 
-export { signupController , loginController, logoutController };
+    // if password is correct log the user in by changing loggedin field to true
+    user.loggedin = true;
+    try {
+      await user.save();
+    } catch (err) {
+      console.error("Error saving user:", err);
+      return res
+        .status(500)
+        .json({ success: false, error: "Failed to update user status" });
+    }
+    console.log(`${user.username} logged in successfully`);
+    return res.status(200).json({ success: true, message: "login successful" });
+  } catch (err) {
+    console.log("Error during login:", err);
+    return res
+      .status(500)
+      .json({ success: false, error: "Internal server error" });
+  }
+};
+
+const logoutController = async (req, res) => {
+  try {
+    const { username, error } = getData(req.body,true);
+    if (error) {
+      return res.status(400).json({ success: false, error });
+    }
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      return res
+        .status(409)
+        .json({ success: false, error: "User does not exist" });
+    }
+    if (!user.loggedin) {
+      return res
+        .status(401)
+        .json({ success: false, error: "User already logged out" });
+    }
+    // if user found and user is logged in log them out
+    user.loggedin = false;
+    try {
+      await user.save();
+    } catch (err) {
+      console.error("Error logging out user:", err);
+      return res
+        .status(500)
+        .json({ success: false, error: "Failed to logout user" });
+    }
+    console.log(`${user.username} logged out successfully`);
+    return res
+      .status(200)
+      .json({ success: true, message: "logout successful" });
+  } catch (err) {
+    console.log("Error during logout:", err);
+    return res
+      .status(500)
+      .json({ success: false, error: "Internal server error" });
+  }
+};
+
+export { signupController, loginController, logoutController };
