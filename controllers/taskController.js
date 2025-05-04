c4a6ba76d18c9031e58f43f780d6e24f42271bf4
Risshi25 <risshirajsen@gmail.com>
2025-05-05 02:25:10 +0530

    fix: improve authentication checks in getTasksController and enhance error handling | check all tasks working

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -2,21 +2,32 @@ import Task from '../models/Task.js';
 import dotenv from "dotenv";
 dotenv.config();
 
+//* Get all the tasks the user has 
 const getTasksController = async (req,res) => {
-    const data = req.user;
-    if (!data) {
+    const { uid } = req.user;
+    if (!uid ) {
         return res.status(401).json({
             success: false,
-            message: 'bad request'
+            error: "Authentication required"
+        });
+    }
+    try {
+        // get all the tasks user has 
+        const userTasks = await Task.findAll({ where : {uid} })
+        return res.status(200).json({
+            success: true,
+            tasks: userTasks
+        })
+    } catch (err ){
+        console.log("Error while retrieving tasks",err)
+        return res.status(500).json({
+            success: false,
+            message: "Error while retrieving tasks"
         })
     }
-    // get all the tasks user has 
-    return res.status(200).json({
-        success: true,
-        message: "successful"
-    })
 }
 
+//* Add a new task for the user
 const addTasksController = async(req,res) =>{
     const { uid, username } = req.user;
     if (!uid || !username) {
